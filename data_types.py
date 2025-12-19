adb00a96699e6e526c21c15acfb8f9acdcde9662
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2025-12-19 17:53:52 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,17 @@
+# integers
+a = 25
+print(a)
+print(type(a))
+#any integer number
+
+#float
+b = 0.2
+print(b)
+print(type(b))
+#any decimal number
+
+#stromg
+c = 'name'
+print(c)
+print(type(c))
+#any letters 
