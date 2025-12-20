701141b0b0d8ac2fbb87804894996abb19510ad1
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2025-12-20 17:45:50 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,83 @@
+# Integers
+a = 25              # Just entering the number is enough 
+print(a)
+print(type(a))
+#Any integer number 
+
+#Float
+a = 0.2             # Entering the number is enough
+print(a)
+print(type(a))
+#Any decimal number
+
+#String
+a = 'name'          # When ever we are taking string '' are importent
+print(a)
+print(type(a))
+#Any letters 
+
+#Complex
+a = 1j              # Having complex 
+print(a)
+print(type(a))
+#Complex type
+
+#Boolean
+a = True            # It will show the actions like True are False
+print(a)
+print(type(a))
+# True and False are called boolean
+
+#Tuple
+a = (1,2.0,'a')     #A Tuple should be in a ()
+print(a)
+print(type(a))
+#It starts from 0. so 1 is in the posstion of 0
+
+#List
+a = [1,2.0,'a']      #A List should be in []
+print(a)
+print(type(a))
+#It also starts with 0
+
+#Set
+a = {'a',2}          #Set shoul be in {}
+print(a)
+print(type(a))
+#It is of letters or numbers
+
+#Dictionary
+a = {'age':23}       #Dictionary is a thing that contains of some value like age=23 or name=23
+print(a)
+print(type(a))
+#If we print age it will be giving it's answer 23
+
+# Range
+a = range(16)        #Range from 0 to ()given 
+print(a)
+print(type(a))
+#It shows the range
+
+#Frozenset
+a = frozenset({'name','place'})    # Frozenset is manly used for mathamatical operation
+print(a)
+print(type(a))
+#It make the frozenset
+
+# Bites
+a = b'name'      #It shows the bites
+print(a)
+print(type(a))
+#The number of bits
+
+# Bytearray
+a = bytearray(1)    #It is a array of bytarray
+print(a)
+print(type(a))
+#The bytearray
+
+# Memoryview
+a = memoryview(bytes(1))   #The memory bytes
+print(a)
+print(type(a))
+#It shows the memory of the given input and its bytes
