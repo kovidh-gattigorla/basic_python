244a561afb2e3d609ec46c7e01cc268a46f405d8
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2025-12-22 14:40:12 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,29 @@
+# The casting is used for converting data type
+
+a = int(1)                  # It is used for direct
+floats = 2.23               # For in-direct
+b = int(floats)             # This will convert float into a integer
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+a = float(3)                #It converts from int to float
+floats = 4.1                #In-direcct comaned
+b = float(floats)           #Direct 
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+a = str('name')             #Direct
+integer = 5                 #In-direct
+b = str(integer)            #Converting int to string
+flo = 6.0                   #In-direct
+c = str(flo)                #Converting float to string
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
