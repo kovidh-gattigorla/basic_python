eb1782c69e453627386eb6b052a83eb9de2fece6
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2025-12-24 18:48:25 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,28 @@
+# If Condition
+a = 10 
+b = 15
+if a>b :                             #This is a conditon of 'If' if it is true then  
+    print('a is grate than b')       #This will be printed 
+print('None')                        #If not this will be printed
+
+
+#Elif conditon
+b = 4
+if b==1 :                              #If then condion
+    print('b is 1')                    #This will be print if not this then
+elif b==2 :                            #This will come under comparision and if yes
+    print('b is 2')                    #This will be print or
+elif b==3 :                            #This condition
+    print('b is 3')                    #This will be printed
+elif b==4 :                            #We can use this elif multiple times
+    print('b is 4')
+
+
+#Else
+marks = 90                  
+if marks<30:                           #If statment is correct for the fiven input
+    print('Fale in exam')              #This will print
+elif 30<marks<85:                      #elif this is correct  
+    print('Pass in exam')              #This will print
+else :                                 #This is the Default action if now of the given condtion is correct
+    print('excelent in exam')          #This will print at the end
