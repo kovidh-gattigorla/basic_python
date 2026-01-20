495dd27b7b7e7d1460c502bdf976fb121cdad97c
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2026-01-20 08:34:30 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+# This is the list
+a = ['a','b','c','d']              #This is the List of strings
+b = [1,2,3,4,5,6,7,8,9]            #This is the List of int
+
+print(a[-1])                       #The -1 comes from last to first of the list
+print(a[2])                        #In list it will start form 0
+print(a[2:4])                      #This will start from 2 to 4 numbers
+print(a[:2])                       #This will end at 2
+print(a[2:])                       #This will start from 2
+print(a[-3:-1])                    #This will be counting from last to 3 to 1
