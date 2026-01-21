b4e8d4f287a605b6becb959eff8393738fc40bcb
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2026-01-21 12:53:40 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -2,9 +2,21 @@
 a = ['a','b','c','d']              #This is the List of strings
 b = [1,2,3,4,5,6,7,8,9]            #This is the List of int
 
-print(a[-1])                       #The -1 comes from last to first of the list
-print(a[2])                        #In list it will start form 0
-print(a[2:4])                      #This will start from 2 to 4 numbers
-print(a[:2])                       #This will end at 2
-print(a[2:])                       #This will start from 2
-print(a[-3:-1])                    #This will be counting from last to 3 to 1
+                 # Accessing the list
+print(a[-1])                       #The -1 comes from last in the list
+print(a[2])                        #In list it will start form 0 so 2 is c
+print(a[1:3])                      #This will start from b to d
+print(a[:2])                       #This will end at 2 means c
+print(a[2:])                       #This will start from 2 means c to end
+print(a[-3:-1])                    #This will be counting from to -3 'b' to 1 'd'
+if 'c' in a :                      #We can use the if condition in the list
+    print('c is in the a list')
+
+                 # Changeing the list
+a[2]='a'                           #Changed the value of 'a' with the 'c'
+print(a)                
+a[2:3] = 'b','c'                   #Changed the values of 'c' to 'b' and 'd' to 'c'
+print(a)
+                 # Adding the new list
+a.insert(4,'e')                    #Inserting the e in 4 position
+print(a)
