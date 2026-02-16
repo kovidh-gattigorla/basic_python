a8769671b55b63a78bd215e1ce6a4692fa9face0
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2026-02-16 18:59:00 +0530

    Add files via upload

--- set.py
+++ set.py
@@ -12,3 +12,21 @@ for b in s:                                            #For finding the variable
     break                                              #Exit from the for loop
 print(2 in s1)                                         #It shows the answer in the boolion
 print('balance' in s3)                                 #Same
+                             #Add the new element
+s.add('i')                                             #For adding the new element
+print(s)
+s.update(s1)                                           #For updating the new set in old set 
+print(s)
+                             #Remove the set elements
+s.remove('h')                                          #To remove selected elements
+print(s)
+s.discard('e')                                         #Alternate of removing the element
+print(s)
+a = s.pop()                                            #Poping or removing of first element storing in another variable
+print(s)
+print(a)
+s.clear()                                              #Clearing the total set
+print(s)
+s = {'a','b','c','d','e','f','g','h'}                 
+del s                                                  #Completely deleting the set
+print(s)
