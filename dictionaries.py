c8a8c351c98e131c5ac2e0fcd565ce2e06dc045d
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2026-02-22 22:34:55 +0530

    Add files via upload

--- dictionaries.py
+++ dictionaries.py
@@ -1,12 +1,12 @@
 d = {'name':'python','clas':'b.tech','rank':1}
 d1 = {'name':'java','clas':'b.tech','rank':[1,2]}                         #We can also store the list in it
-d2 = dict(name='c', clas='b.tech', rank=[1,2,3])
+d2 = dict(name='c', clas='b.tech', rank=(1,2,30))                         #We can also store the list in it
 print(d)                                                                     
 print(d1)
 print(d2)
 print(type(d))                                                             #Type is Dictionarie
 print(len(d))
-                                    #Accessing the dictionaries
+                                     #Accessing the dictionaries
 print(d['name'])                                                           #It is like name = python if we print name then python or voice versa
 a = d1.get('rank')                                                         #by takig the element and get 
 print(a)
@@ -18,3 +18,13 @@ d = d.items()                                                              #Ever
 print(d)
 if 'rank' in d1:                                                            #If condition
     print('Yes it is True')
+                                     #Changeing Item
+d1['rank']=3                                                               #Changing the value
+print(d1)
+d1.update({'rank':10})                                                     #Changed by update
+print(d1)
+                                     #Adding Item
+d1['type']='easy'                                                          #Added the new key and value
+print(d1)
+d1.update({'level':10})
+print(d1)
